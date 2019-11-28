b84cd77d1604d3625c5685002540d19010144a74
joaoh <joao_camargo10@hotmail.com>
2019-11-28 14:54:33 -0300

    Background image

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,55 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        appBar: _buildAppBar(),
+        body: _buildBody(),
+      ),
+    );
+  }
+
+  _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.black.withOpacity(0.9),
+      leading: IconButton(
+        icon: Icon(Icons.menu),
+        onPressed: () {},
+      ),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  _buildBody() {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/background.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Container(
+        decoration: BoxDecoration(color: Colors.black.withOpacity(0.79)),
+        child: ListView(
+          children: <Widget>[
+            // criar método para cada item
+          ],
+        ),
+      ),
+    );
   }
 }
